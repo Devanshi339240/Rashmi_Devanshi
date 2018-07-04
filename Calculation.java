cbc9736d2db35558691e2cf9f0d18165647a054c
Devanshi339240 <40818892+Devanshi339240@users.noreply.github.com>
2018-07-04 14:03:53 +0530

    Update Calculation.java

--- Calculation.java
+++ Calculation.java
@@ -4,9 +4,11 @@ public class Calculation {
   
     public static int findMax(int arr[]){  
         int max=arr[0];  
+        int x=0;
         for(int i=1;i<arr.length;i++){  
             if(max<arr[i])  
                 max=arr[i];  
+          x++;
         }  
         return max;  
     }  
