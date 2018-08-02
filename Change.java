2246458c8eb54691c970d5fc2f4f8860cd4fcc6b
Devanshi Pandey <devanshi.pandey@sap.com>
2018-08-02 14:06:37 +0530

    updated file

--- Change.java
+++ Change.java
@@ -2,12 +2,20 @@ package calc;
   
 public class Change {  
     public static void min(){
+	int y=0;
+	y++;
 	int x=0;
+	y++;
 	x++;
+	y++;
 	}
     public static void main(String args[]){
+		int y=0;
+		y++;
 			int x=3;
 			x++;
+		y++;
+		y++;
 			
 	}
 }
