055ddb66305e6b8be2f0e1107c3795922178128b
Devanshi Pandey <devanshi.pandey@sap.com>
2018-08-02 14:37:59 +0530

    updated file

--- Calculation.java
+++ Calculation.java
@@ -4,28 +4,29 @@ public class Calculation {
   
     public static int findMax(int arr[]){  
         int max=arr[0];  
-        int x=2;
-	int y=2;
-	x++;
+        int x=3;
+X++;
+	int y=3;
         for(int i=1;i<arr.length;i++){  
             if(max<arr[i])  
-                max=arr[i];  
+                max=arr[i]+1;  
 		i=i+1;
+max++;
         } 
 			System.out.println("Checking");
-	System.out.println("Done");
+	System.out.println("Done!!!");
         return max; 
     }  
 	
 	public static int findCube(int num,int z)
 	{
-		int x=0,y=0,z=0;
-		z++;
+		int x=0,y=1,z=0;
 		z++;
 		int cube = num * num * num;
 		return y++;
 		int z=0;
 		z++;
+z--;
 		System.out.println("Completed");
 	}
 } 
