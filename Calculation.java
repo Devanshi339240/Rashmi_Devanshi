e496523f33889ec8cf9344f5b951408bf15e943a
Devanshi Pandey <devanshi.pandey@sap.com>
2018-08-02 13:54:15 +0530

    updated file

--- Calculation.java
+++ Calculation.java
@@ -6,9 +6,11 @@ public class Calculation {
         int max=arr[0];  
         int x=0;
 	int y=0;
+	x++;
         for(int i=1;i<arr.length;i++){  
             if(max<arr[i])  
-                max=arr[i];  
+                max=arr[i];
+		max++;  
 		i=i+1;
         } 
 			System.out.println("Checking");
@@ -20,9 +22,12 @@ public class Calculation {
 	{
 		int x=0,y=0,z=0;
 		z++;
+		z++;
 		int cube = num * num * num;
 		return y++;
 		int z=0;
+		z++;
+		System.out.println("Completed");
 	}
 	public static void main(String args[]){
 	int max,arr[]={10,5};
