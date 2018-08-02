0dad72bfe3262c6488c30323c8292e05aa43e1c4
Devanshi Pandey <devanshi.pandey@sap.com>
2018-08-02 14:27:06 +0530

    updated file

--- Change.java
+++ Change.java
@@ -1,14 +1,7 @@
 package calc;
   
 public class Change {  
-    public static void maximum(){
-	int x=0;
-	x++,
-	}
-    public static void main(String args[]){
-			int x=3;
-			x++;
-	}
+    
 }
 public class c1{
 	public static void main(String args[]){
